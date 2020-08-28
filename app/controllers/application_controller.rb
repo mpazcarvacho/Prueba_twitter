8040b0600d1edcd35c7b7d85341d48e74b9ba288
mariapaz <mpazcarvacho@gmail.com>
2020-08-28 07:05:23 -0400

    navbar, modelo user y devise añadidos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :pic_url])
+  end
+  
 end
