9ac35ed7d6a1d34d4330eaaad91d5e47da38c337
mariapaz <mpazcarvacho@gmail.com>
2020-09-04 20:36:41 -0400

    user profile route added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def follow(user_id)
   
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
-  get 'users/user'
+  
   resources :tweets do
     post 'likes', to:"tweets#likes"
     post 'retweets', to:"tweets#retweets"
   end
   devise_for :users
   
-  
+  resources :users
+  post 'follow/:user_id', to: 'users#follow', as: 'follow'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
