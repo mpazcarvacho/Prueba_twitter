b6a22f0e228d2064d8baae0dff8bc99e4ba3199b
mariapaz <mpazcarvacho@gmail.com>
2020-09-05 07:06:05 -0400

    apparently activeadmin issue free

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #SOLVE ACTIVEADMIN ISSUE
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
