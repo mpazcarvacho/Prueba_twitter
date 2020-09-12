9f65788e3d165ce54afee061476e38e78f9444e3
mariapaz <mpazcarvacho@gmail.com>
2020-09-12 04:12:17 -0300

    admin access fixed on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_admin!, only: :admin
   protect_from_forgery with: :exception
 
   before_action :configure_permited_parameters, if: :devise_controller?
 
   def authenticate_admin!
     if current_user.nil?
+      
+    else
       if !current_user.admin
         redirect_to new_user_session_path
       end
     end
+
   end
 
   protected
